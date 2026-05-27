87aa8409e19e71f1249ca30cc1751e7ba10e58c2
bsfrawat02-cloud <bsfrawat02@gmail.com>
2026-05-27 07:01:06 +0530

    Update ask.js

--- api/ask.js
+++ api/ask.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   try {
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCTjyb-Xi4XY_tPT5zZPBCkZL-J7xHQx4Q",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAinQScQQ-6U84A08jwIOyaH3sNP2CAjPs",
       {
         method: "POST",
         headers: {
